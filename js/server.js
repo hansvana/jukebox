2fdfe11064621d1dc1807a27ecdce70dead111f5
hansvana <hansvanarken@gmail.com>
2016-03-05 20:09:52 +0100

    better file structure, now I actually know what I'm doing

--- js/server.js
+++ js/server.js
@@ -1,10 +1,9 @@
 var express = require('express');
 var bodyParser = require('body-parser')
-var fs = require('fs');
 var app = express();
 
 app.use( bodyParser.json() );
-
+app.use( express.static('public') );
 
 var fileList = [];
 
@@ -23,22 +22,10 @@ var allowCrossDomain = function(req, res, next) {
 };
 app.use(allowCrossDomain);
 
-
-fs.readdir('mp3', (err, files) => {
-    if (err) throw err;
-    files = files.filter( file => {
-        return (file.split('.')[1] === 'mp3');
-    }).map( file => {
-        return file.split('.')[0];
-    });
-    fileList = files.slice();
-    console.log("found " + fileList.length + " files.")
-});
-
 app.post('/fuzzySearch', function (req, res) {
     console.log(req.body);
     var results = fileList.filter( file => {
-        return ( file.indexOf(req.body.txt) !== -1 );
+        return ( file.nameWithoutExtension.indexOf(req.body.txt) !== -1 );
     });
 
     res.setHeader('Content-Type', 'application/json');
@@ -55,13 +42,31 @@ app.post('/sendRequest', function (req, res) {
 app.post('/setFileList', function (req, res) {
     var nFiles = req.body.length;
     console.log("Received " + nFiles + " file names");
-    
+
+    var i = 0;
+    for(var file of req.body){
+        fileList.push({
+            key: i++,
+            name: file,
+            nameWithoutExtension: file.split('.')[0],
+        })
+    }
+
     res.setHeader('Content-Type', 'application/json');
     res.send(JSON.stringify({ type: "success", text: "Received " + nFiles + " file names" }));
 });
 
 app.get('/', function (req, res) {
-    res.send("Hello World!");
+    res.write("<h1>File List</h1>");
+    res.write("<table>");
+    for (var file of fileList){
+        res.write("<tr>" +
+                 "<td>"+file.key+"</td>" +
+                 "<td>"+file.name+"</td>" +
+                 "<td>"+file.nameWithoutExtension+"</td>" +
+                 "</tr>");
+    }
+    res.end("</table>");
 });
 
 app.listen(3000, function () {
